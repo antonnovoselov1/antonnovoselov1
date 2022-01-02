882749928571fa58b86527b0dbbd0b86975cb7fe
Rodion Shkokov <72241649+rodion02@users.noreply.github.com>
2022-01-02 19:09:12 +0700

    Сортировка расчёской

---
+++ class_work/Сортировки/hairbrush_sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <Windows.h>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    int N = 15;
+    int arr[15] = {1, 14, 89, 71, 13, 8, 3, 0, 29, 54, 53, 68, 10, 101, 37};
+
+    int step=N-1;
+    while(step>=1) {
+        for (int i = 0; i+step < N; i++)
+            if(arr[i]>arr[i+step])
+                swap(arr[i],arr[i+step]);
+        step=(int)(step/1.247);
+
+    }
+    for (int i=0;i<N;i++)
+        cout<<arr[i]<<" ";
+    cout<<endl<<clock()/1000.0;
+    return 0;
+}
